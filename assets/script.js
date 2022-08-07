3c57c3cb04218e33c67b52e4e65519b6659a2361
Munnos <munnos@hotmail.co.uk>
2022-08-07 16:21:24 +0100

    troubleshoot API

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,32 @@
-var placeSearch = document.getElementById('exampleInputPlace');
+// API data need to get:
+
+// City
+// Date
+// icon rep of weather conditions
+// temperature
+// humidity
+// wind speed
+// UV index
+// 5 day forecast for all of the above
+
+
+
+
+
 var fetchButton = document.getElementById('fetch-button');
+var placesearch = document.getElementById('place-input');
 
-function getApi() {
 
-    var requestUrl = "https://api.openweathermap.org/data";
+
+fetchButton.addEventListener('click', getcityApi);
+
+function getcityApi() {
+    var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + placesearchValue + "&limit=5&appid=d2d3af92279205e005384f665435892b";
+   
+    var placesearchValue = document.getElementById('place-input').value;
+
 
     console.log(requestUrl);
 
-    fetch(requestUrl)
+    // fetch(requestUrl)
 }
-
-fetchButton.addEventListener('click', getApi);

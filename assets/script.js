477762592854e444b6331e6f34a25c890e5e2e99
Munnos <munnos@hotmail.co.uk>
2022-08-07 14:35:29 +0100

    changes to css

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,11 @@ var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
 
-    
+    var requestUrl = "https://api.openweathermap.org/data";
+
+    console.log(requestUrl);
+
+    fetch(requestUrl)
 }
+
+fetchButton.addEventListener('click', getApi);
